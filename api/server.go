e61e1dffe0dee83d2e68f1233266f4e0d3130988
Omar Kohl <omarkohl@posteo.net>
2022-11-10 08:14:06 +0100

    Revert "Disable CORS to verify the request then fails"
    This reverts commit 11a00b070a1ba50a2922a5075fdbb911161f2e58.

--- api/server.go
+++ api/server.go
@@ -7,13 +7,14 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/rs/cors"
 )
 
 func main() {
 	r := chi.NewRouter()
 
 	// Use default options
-	//r.Use(cors.Default().Handler)
+	r.Use(cors.Default().Handler)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
